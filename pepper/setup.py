0792d1fef1655fc41cce0ab7b4e73a7841928574
JakubCiesko <jakub.ciesko@gmail.com>
2025-11-24 16:39:17 +0100

    Refactor setup.py to read dependencies from requirements.txt and add missing requirements files

---
+++ pepper/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+with open("requirements.txt") as f:
+    requirements = f.read().splitlines()
+    
+setup(
+    name="pepper_client",
+    version="0.1.0",
+    packages=find_packages(),
+    install_requires= requirements, # install pynaoqi manually according to this manual: https://nlp.fi.muni.cz/trac/pepper/wiki/InstallationInstructions
+    python_requires=">=2.7,<3", # pepper supports python 2 only! 
+)
